cf7a33e9e18d40da3ccda3d24469525ff4a841e1
Edson Junior <101193595+edsondiasjunior@users.noreply.github.com>
2025-07-25 16:03:38 -0300

    teste.js

---
+++ tes.js
@@ -0,0 +1,8 @@
+export default function render(element) {
+  element.innerHTML = `
+    <div style="padding: 16px; border: 1px solid #ccc;">
+      <h2>Bem-vindo!</h2>
+      <p>Este é um conteúdo HTML customizado com JavaScript ES Module.</p>
+    </div>
+  `;
+}
